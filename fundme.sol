f43db0f1865152f1ee688c95d682817ce7166ced
timg0x <103272632+timg0x@users.noreply.github.com>
2022-06-09 19:28:01 -0400

    Update fundme.sol

--- fundme.sol
+++ fundme.sol
@@ -5,28 +5,48 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
     uint256 public minimumUsd = 10 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!");
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function getPrice() public view returns(uint256) {
-    // Rinkby test net
-    // ABI
-    // Address 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
-    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-    (,int256 price,,,) = priceFeed.latestRoundData();
-    return uint256(price * 1e10);
+    function withdraw() public onlyOwner { 
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
     }
-
-    //function withdraw(){}
 }
